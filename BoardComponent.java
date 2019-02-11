d739272aa618e83b2fa0bf36426410e9ddfe8a92
shreyr10 <44396929+shreyr10@users.noreply.github.com>
2019-02-11 13:16:05 +0000

    Update BoardComponent.java

--- BoardComponent.java
+++ BoardComponent.java
@@ -130,6 +130,145 @@ public class BoardComponent extends JComponent{
 		int fontSize = 20;
 	    g.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
 	    g.setColor(Color.black);
+		
+		g.drawString("7", 310, 450);
+		g.drawString("8", 260, 450);
+		g.drawString("9", 210, 450);
+		g.drawString("10", 145, 450);
+		g.drawString("11", 90, 450);
+		g.drawString("12", 40, 450);
+
+		g.drawString("24", 650, 30);
+		g.drawString("23", 600, 30);
+		g.drawString("22", 540, 30);
+		g.drawString("21", 490, 30);
+		g.drawString("20", 440, 30);
+		g.drawString("19", 390, 30);
+		g.drawString("18", 310, 30);
+		g.drawString("17", 260, 30);
+		g.drawString("16", 205, 30);
+		g.drawString("15", 145, 30);
+		g.drawString("14", 90, 30);
+		g.drawString("13", 40, 30);
+
+		g.drawOval(30, 390, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(30, 390, 38, 38);
+
+		g.drawOval(30, 352, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(30, 352, 38, 38);
+
+		g.drawOval(30, 314, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(30, 314, 38, 38);
+
+		g.drawOval(30, 276, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(30, 276, 38, 38);
+
+		g.drawOval(30, 238, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(30, 238, 38, 38);
+
+		g.drawOval(30, 35, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(30, 35, 38, 38);
+
+		g.drawOval(30, 73, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(30, 73, 38, 38);
+
+		g.drawOval(30, 111, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(30, 111, 38, 38);
+
+		g.drawOval(30, 149, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(30, 149, 38, 38);
+
+		g.drawOval(370, 35, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(370, 35, 38, 38);
+
+		g.drawOval(370, 73, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(370, 73, 38, 38);
+
+		g.drawOval(370, 111, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(370, 111, 38, 38);
+
+		g.drawOval(370, 149, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(370, 149, 38, 38);
+
+		g.drawOval(370, 187, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(370, 187, 38, 38);
+
+		g.drawOval(370, 390, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(370, 390, 38, 38);
+
+		g.drawOval(370, 352, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(370, 352, 38, 38);
+
+		g.drawOval(370, 314, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(370, 314, 38, 38);
+
+		g.drawOval(370, 276, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(370, 276, 38, 38);
+
+		g.drawOval(370, 238, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(370, 238, 38, 38);
+
+		g.drawOval(240, 390, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(240, 390, 38, 38);
+
+		g.drawOval(240, 352, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(240, 352, 38, 38);
+
+		g.drawOval(240, 314, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(240, 314, 38, 38);
+
+		g.drawOval(240, 35, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(240, 35, 38, 38);
+
+		g.drawOval(240, 73, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(240, 73, 38, 38);
+
+		g.drawOval(240, 111, 38, 38);
+		g.setColor(Color.ORANGE);
+		g.fillOval(240, 111, 38, 38);
+
+		g.drawOval(640, 35, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(640, 35, 38, 38);
+
+		g.drawOval(640, 73, 38, 38);
+		g.setColor(Color.red);
+		g.fillOval(640, 73, 38, 38);
+
+		g.drawOval(640, 390, 38, 38);
+		g.setColor(Color.orange);
+		g.fillOval(640, 390, 38, 38);
+
+		g.drawOval(640, 352, 38, 38);
+		g.setColor(Color.orange);
+		g.fillOval(640, 352, 38, 38);
+	}
+
+}
 	   
 	   
 		
